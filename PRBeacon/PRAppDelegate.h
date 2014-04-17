d0efcadbdd40075095975e9a6064d1e8a030eb17
Paul Robinson <paul@coursera.org>
2014-04-17 12:04:40 -0700

    Initial Commit

---
+++ PRBeacon/PRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRAppDelegate.h
+//  PRBeacon
+//
+//  Created by Paul Robinson on 4/17/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PRBeacon/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PRBeacon
+//
+//  Created by Paul Robinson on 4/17/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRAppDelegate class]));
+    }
+}
